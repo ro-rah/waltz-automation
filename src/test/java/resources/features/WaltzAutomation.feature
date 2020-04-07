1db8890c013b5e8d363837d717b950ba321fde1d
prateek <prateek.punhani@gmail.com>
2020-04-07 19:18:29 +0100

    added scenarios

--- src/test/java/resources/features/WaltzAutomation.feature
+++ src/test/java/resources/features/WaltzAutomation.feature
@@ -1,9 +1,9 @@
-
-Feature: Waltz automation
+  @sanity
+  Feature: Waltz automation
 
   Scenario: Search an Application
     Given i am on waltz home page
-    When i want to search application "Bombay"
+    When i search application "Bombay"
     Then application details should be displayed
     And view the "Attestations" associated wth it
     And view the "Bookmarks" associated wth it
@@ -13,15 +13,30 @@ Feature: Waltz automation
     And view the "Notes" associated wth it
     And view the "Diagrams" associated wth it
     And view the "Change Initiatives" associated wth it
-  @sanity
+
   Scenario: create a tag
     Given i am on waltz home page
-    When i want to search application "Bombay"
+    When i search application "Bombay"
     Then application details should be displayed
     And create a tag "Sealights"
 
-    Scenario: remove a tag
-      Given i am on waltz home page
-      When i want to search application "Bombay"
-      Then application details should be displayed
-      And remove tag "Sealights"
+  Scenario: remove a tag
+    Given i am on waltz home page
+    When i search application "Bombay"
+    Then application details should be displayed
+    And remove tag "Sealights"
+
+  Scenario: create an attestation
+    Given i am on waltz home page
+    When i search application "Bombay"
+    Then application details should be displayed
+    And  attest the physical flow data
+
+  Scenario: view logical flow of org unit
+    Given i am on waltz home page
+    When i want to  view logical flow for "Risk It" Organisational unit
+    Then summary for logical flow should be displayed
+    And view graphical representation
+    And view  tabular representation
+
+
--- src/test/java/stepDefinition/StepDefinition.java
+++ src/test/java/stepDefinition/StepDefinition.java
@@ -2,6 +2,7 @@ package stepDefinition;
 
 import static org.junit.Assert.assertTrue;
 
+import io.cucumber.core.internal.gherkin.SymbolCounter;
 import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -17,31 +18,31 @@ import java.util.concurrent.TimeUnit;
 /**
  * Unit test for simple App.
  */
-public class StepDefinition
-{
+public class StepDefinition {
     @After
-            public void closeBrowser(){
-    //  driver.quit();
+    public void closeBrowser() {
+          driver.quit();
     }
 
     WebDriver driver;
+
     @Given("i am on waltz home page")
     public void i_am_on_waltz_home_page() {
-    System.out.println("launch waltz");
-    System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
-        driver= new ChromeDriver();
+        System.out.println("launch waltz");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        driver = new ChromeDriver();
         driver.get("http://ec2-34-208-125-3.us-west-2.compute.amazonaws.com:8081/waltz-web/home");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @When("i want to search application {string}")
-    public void i_want_to_search_application(String string) {
+    @When("i search application {string}")
+    public void iSearchApplication(String string) {
         System.out.println("Search Application");
         driver.findElement(By.xpath("//a[text()=' Search ']")).click();
         driver.findElement(By.xpath("//input[@type='search']")).click();
         driver.findElement(By.xpath("//input[@type='search']")).sendKeys(string);
-        driver.findElement(By.xpath("//span[contains(text(),'"+string+"')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'" + string + "')]")).click();
         //driver.findElement(By.xpath("//a[contains(text(),'View')]")).click();
     }
 
@@ -52,14 +53,14 @@ public class StepDefinition
 
     @Then("view the {string} associated wth it")
     public void view_the_associated_wth_it(String string) {
-        System.out.println("view "+string);
-        driver.findElement(By.xpath("//span[text()='"+string+"']")).click();
+        System.out.println("view " + string);
+        driver.findElement(By.xpath("//span[text()='" + string + "']")).click();
     }
 
 
     @And("create a tag")
     public void createATag() {
-    System.out.println("tag created");
+        System.out.println("tag created");
     }
 
     @When("i want to search {string}")
@@ -86,4 +87,48 @@ public class StepDefinition
         driver.findElement(By.xpath("//div[contains(text(),'Tags')]")).click();
         driver.findElement(By.xpath("//button[@class='btn btn-xs']")).click();
     }
+
+    @And("attest the physical flow data")
+    public void attestThePhysicalFlowData() {
+        driver.findElement(By.xpath("//span[text()='Attestations']")).click();
+        boolean varFirst = false;
+        System.out.println(varFirst);
+        driver.findElement(By.xpath("//a[text()='Attest physical flows']")).click();
+        driver.findElement(By.xpath("//button[contains(text(),'Confirm')]")).click();
+        driver.switchTo().alert().accept();
+    }
+
+
+    @When("i want to  view logical flow for {string} Organisational unit")
+    public void iWantToViewLogicalFlowForOrganisationalUnit(String arg0) {
+        driver.findElement(By.xpath("//span[text()='Org Units']")).click();
+        driver.findElement(By.xpath("//span//span[text()='Risk IT']")).click();
+    }
+
+    @Then("summary for logical flow should be displayed")
+    public void summaryForLogicalFlowShouldBeDisplayed() {
+        driver.findElement(By.xpath("//span[text()='Logical Flows']")).click();
+
+    }
+
+    @And("view graphical representation")
+    public void viewGraphicalRepresentation() {
+        driver.findElement(By.xpath("//a[text()='Graph']")).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @And("view  tabular representation")
+    public void viewTabularRepresentation() {
+        driver.findElement(By.xpath("//a[text()='Table']")).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
